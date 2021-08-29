cf99451f6f91eb7b58abe7f20938ec312496ade4
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-08-30 01:05:56 +0300

    add list markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import foodCardsTpl from './templates/food-cards.hbs';
+import foods from './menu.json'
 import './sass/main.scss';
+
+const refs = {
+    list: document.querySelector('.js-menu'),
+};
+
+const foodCardsMarkUp = foodCardsMarkUpCreate(foods);
+
+refs.list.insertAdjacentHTML('beforeend', foodCardsMarkUp);
+
+function foodCardsMarkUpCreate(arr) {
+    return arr.map(foodCardsTpl).join('');
+};
